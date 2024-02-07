b17ec73aa3ae66989f027d0f21ad4d869f2732db
Swathi <swathiasok14@gmail.com>
2024-02-06 18:18:19 -0800

    Adding Assignment 2 solution

---
+++ Assignment2_solution/app.py
@@ -0,0 +1,104 @@
+from flask import Flask, request, render_template, jsonify
+from datetime import *
+from dateutil.relativedelta import *
+import finnhub
+import requests
+
+app = Flask(__name__)
+app.debug = True
+finnhub_client = finnhub.Client(
+    api_key="cmr19q1r01ql2lmtdgi0cmr19q1r01ql2lmtdgig")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/company_data', methods=['POST'])
+def company():
+    stock_symbol = request.get_data(as_text=True)
+    response = finnhub_client.company_profile2(symbol=stock_symbol)
+    if response:
+        value = {
+            "logo": response.get('logo'),
+            "name": response.get('name'),
+            "symbol": stock_symbol.upper(),
+            "exchange_code": response.get('exchange'),
+            "start_date": response.get('ipo'),
+            "category": response.get('finnhubIndustry')
+        }
+    else:
+        value = 'None'
+
+    return value
+
+
+@app.route('/stock_data', methods=["POST"])
+def stocks():
+    stock_symbol = request.get_data(as_text=True)
+    response = finnhub_client.quote(symbol=stock_symbol)
+    response_2 = finnhub_client.recommendation_trends(symbol=stock_symbol)
+
+    value = {
+        "symbol": stock_symbol.upper(),
+        "trading_day": datetime.utcfromtimestamp(response.get('t')).strftime('%d %B, %Y'),
+        "closing_price": response.get('pc'),
+        "opening_price": response.get('o'),
+        "high_price": response.get('h'),
+        "low_price": response.get('l'),
+        "change": response.get('d'),
+        "change_percent": response.get('dp'),
+        "strong_sell": response_2[0].get("strongSell"),
+        "strong_buy": response_2[0].get("strongBuy"),
+        "buy": response_2[0].get("buy"),
+        "sell": response_2[0].get("sell"),
+        "hold": response_2[0].get("hold")
+    }
+
+    return value
+
+
+@app.route('/charts_data', methods=['POST'])
+def charts():
+    stock_symbol = request.get_data(as_text=True)
+    today = date.today()
+    from_date = today+relativedelta(months=-6, days=-1)
+    url = "https://api.polygon.io/v2/aggs/ticker/" + stock_symbol.upper() + \
+        "/range/1/day/" + str(from_date) + "/" + str(today) + \
+        "?adjusted=true&sort=asc&apiKey=6xsTW8rqr6AQMKhkoNuwA2U54Z7bDJLd"
+
+    price_array = []
+    volume_array = []
+
+    responses = requests.get(url)
+
+    for response in responses.json().get("results"):
+        price_array.append([response.get("t"), response.get("c")])
+        volume_array.append([response.get("t"), response.get("v")])
+
+    return jsonify({'price_array': price_array, 'volume_array': volume_array})
+
+
+@app.route('/news_data', methods=['POST'])
+def news():
+    stock_symbol = request.get_data(as_text=True)
+    today = date.today()
+    last_month = today+relativedelta(months=-1)
+    responses = finnhub_client.company_news(
+        symbol=stock_symbol, _from=last_month, to=today)
+
+    print(responses[0])
+    values = []
+
+    for response in responses:
+        if response.get('headline') and response.get('datetime') and response.get('url') and response.get('image'):
+            response['datetime'] = datetime.utcfromtimestamp(
+                response.get('datetime')).strftime('%d %B, %Y')
+            values.append(response)
+
+    return values
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ Assignment2_solution/static/script.js
@@ -0,0 +1,272 @@
+function getData(tab) {
+    let stock_symbol = document.getElementById('smbl').value;
+
+    if (stock_symbol) {
+        var xml_req = new XMLHttpRequest;
+
+        xml_req.open('POST', `/${tab}_data`, true);
+        xml_req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+
+        xml_req.send(stock_symbol);
+
+        xml_req.onreadystatechange = () => {
+            if (xml_req.status == 200) {
+                var response = xml_req.responseText;
+                if (response != "None") {
+                    var res = response && JSON.parse(response);
+                    if (res && typeof res.name != null) {
+                        document.getElementById("empty-div").style.display = "none";
+                        document.getElementById("display").style.display = "block";
+                        document.getElementById("empty-symbol").style.display = "none";
+                        switch (tab) {
+                            case "company":
+                                showCompany(res);
+                                break;
+                            case "stock":
+                                showStock(res);
+                                break;
+                            case "charts":
+                                showChart(res);
+                                break;
+                            case "news":
+                                showNews(res);
+                                break;
+                        }
+                    }
+                } else {
+                    document.getElementById("display").style.display = "none";
+                    document.getElementById("empty-div").style.display = "flex";
+                }
+            }
+        }
+
+    } else {
+        console.log('no input');
+        document.getElementById("display").style.display = "none";
+        document.getElementById("empty-symbol").style.display = "flex";
+    }
+
+    return false;
+}
+
+function clearForm() {
+    document.getElementById("smbl").value = "";
+    document.getElementById("display").style.display = "none";
+    document.getElementById("empty-div").style.display = "none";
+}
+
+function hideTabs(currentTab) {
+    tabs = ["company", "stock", "chart", "news"]
+    tabs.forEach(element => {
+        if (element != currentTab) {
+            document.getElementById(element).style.display = "none";
+            document.getElementById(`${element}_link`).classList.remove("active");
+        }
+    });
+
+    document.getElementById(currentTab).style.display = "block";
+    document.getElementById(`${currentTab}_link`).classList.add("active");
+    document.getElementById("empty-symbol").style.display = "none";
+}
+
+function showCompany(response) {
+    event.preventDefault();
+    hideTabs("company");
+    document.getElementById("company_logo").src = response.logo;
+    document.getElementById("company_name").textContent = response.name;
+    document.getElementById("company_symbol").textContent = response.symbol;
+    document.getElementById("company_code").textContent = response.exchange_code;
+    document.getElementById("company_start_date").textContent = response.start_date;
+    document.getElementById("company_category").textContent = response.category;
+}
+
+function showStock(response) {
+    event.preventDefault();
+    hideTabs("stock");
+
+    document.getElementById("stock_symbol").textContent = response.symbol;
+    document.getElementById("trading_day").textContent = response.trading_day;
+    document.getElementById("closing_price").textContent = response.closing_price;
+    document.getElementById("opening_price").textContent = response.opening_price;
+    document.getElementById("high_price").textContent = response.high_price;
+    document.getElementById("low_price").textContent = response.low_price;
+
+    if (response.change > 0) {
+        document.getElementById("change").innerHTML = `${response.change}<img id="change_percent_img" style="vertical-align: middle;" src="/static/img/GreenArrowUp.png">`;
+        document.getElementById("change_percent").innerHTML = `${response.change_percent}<img id="change_percent_img" style="vertical-align: middle;" src="/static/img/GreenArrowUp.png">`;
+
+    } else {
+        document.getElementById("change").innerHTML = `${response.change}<img id="change_percent_img" style="vertical-align: middle;" src="/static/img/RedArrowDown.png">`;
+        document.getElementById("change_percent").innerHTML = `${response.change_percent}<img id="change_percent_img" style="vertical-align: middle;" src="/static/img/RedArrowDown.png">`;
+    }
+
+    document.getElementById("strong_sell").textContent = response.strong_sell;
+    document.getElementById("strong_buy").textContent = response.strong_buy;
+    document.getElementById("buy").textContent = response.buy;
+    document.getElementById("hold").textContent = response.hold;
+    document.getElementById("sell").textContent = response.sell;
+
+}
+
+function buildChart(data) {
+
+    volume_data = data.volume_array
+    price_data = data.price_array
+
+    const stock_price = [];
+    const volume = [];
+    const dataLength = volume_data.length;
+    let stock_symbol = document.getElementById('smbl').value;
+
+    let currentDate = new Date().toJSON().slice(0, 10);
+
+    for (let i = 0; i < dataLength; i += 1) {
+
+        volume.push([
+            volume_data[i][0],
+            volume_data[i][1]
+        ]);
+
+        stock_price.push([
+            price_data[i][0],
+            price_data[i][1]
+        ]);
+    }
+
+    Highcharts.stockChart('chart-container', {
+
+        chart: {
+            events: {
+                load: function () {
+                    let inputGroup = document.querySelector('.highcharts-input-group');
+                    inputGroup.remove();
+                }
+            }
+        },
+
+        rangeSelector: {
+            selected: 4,
+            buttons: [{
+                type: 'day',
+                count: 7,
+                text: '7d',
+            }, {
+                type: 'day',
+                count: 15,
+                text: '15d'
+            }, {
+                type: 'month',
+                count: 1,
+                text: '1m'
+            }, {
+                type: 'month',
+                count: 3,
+                text: '3m'
+            }, {
+                type: 'month',
+                count: 6,
+                text: '6m'
+            }]
+        },
+
+        title: {
+            text: `Stock Price ${stock_symbol} ${currentDate}`
+        },
+
+        subtitle: {
+            useHTML: true,
+            text: '<a href = "https://polygon.io"> Source: Polygon.io </a>',
+
+        },
+
+        yAxis: [{
+                title: {
+                    text: 'Stock Price'
+                },
+                tickAmount: 6,
+                opposite: false,
+            },
+            {
+                title: {
+                    text: 'Volume'
+                },
+                tickAmount: 6,
+            }
+        ],
+
+        tooltip: {
+            split: true
+        },
+
+        series: [{
+            name: 'AAPL Stock Price',
+            data: stock_price,
+            type: 'area',
+            threshold: null,
+            tooltip: {
+                valueDecimals: 2
+            },
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            }
+        }, {
+            type: 'column',
+            name: 'Volume',
+            data: volume,
+            yAxis: 1,
+            pointWidth: 5,
+            grouping: false,
+            color: '#000104'
+        }]
+    });
+}
+
+
+function showChart(response) {
+    event.preventDefault();
+    hideTabs("chart");
+
+    buildChart(response);
+}
+
+function newsRow(response) {
+    return `
+    <div class="news-row">
+        <img src="${response.image}" class="news-image">
+        <div class="news-content">
+            <p class="news-title">${response.headline}</p>
+            <p class="news-description">${response.datetime}</p>
+            <a href="${response.url}" class="news-link">See Original Post</a>
+        </div>
+    </div>
+    `;
+}
+
+function showNews(response) {
+    event.preventDefault();
+    hideTabs("news");
+    document.getElementById("news").innerHTML = ""
+    for (i = 0; i < 5; i++) {
+        document.getElementById("news").innerHTML += newsRow(response[i]);
+    }
+}
+
+function submitForm() {
+
+    if (document.getElementById('smbl').checkValidity()) {
+        getData('company');
+    } else {
+        console.log('here2');
+        form.reportValidity();
+        return false;
+    }
+}
